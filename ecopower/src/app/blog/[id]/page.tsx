08b68d68390b164a8063e056ea43915dd7b5ec1a
MAIA332 <lk.maia0505@outlook.com>
2024-11-06 11:38:07 -0300

    Texts and images

--- ecopower/src/app/blog/[id]/page.tsx
+++ ecopower/src/app/blog/[id]/page.tsx
@@ -4,31 +4,38 @@ import React from 'react'
 import Navbar from '../../components/navbar'
 import Submenu from '../../components/submenu'
 import cards from "../../../../cards.json"
-import { useParams } from 'next/navigation'; // Import from next/navigation
+import { useParams } from 'next/navigation';
 
 function Blog() {
-    const { id } = useParams(); // Use the new useParams hook
+    const { id } = useParams();
 
     const card = cards.find((card) => card.id === id);
 
     if (!card) return <p>Card não encontrado.</p>;
 
     return (
-        <div className="bg-slate-100 h-screen">
+        <div className="bg-slate-100">
             <Navbar />
             <Submenu />
-            <div className="bg-black pt-20">
-                <div className="max-w-screen-sm mx-auto py-10 h-screen">
-                    <h1 className="text-4xl font-bold leading-tight mb-4 text-slate-100">
-                        {card.title}
+            <div className="bg-slate-950 pt-20 h-full">
+                <div className="max-w-screen-sm mx-auto py-10 h-screen overflow-auto">
+                    <h1 className="text-4xl font-bold leading-tight mb-6 text-slate-100">
+                    {card.title}
                     </h1>
                     <p className="text-sm text-gray-400 mb-6">
-                        Por <span className="text-white font-bold">{card.author}</span> em {card.data}
+                    Por <span className="text-white font-bold">{card.author}</span> em {card.data}
                     </p>
-                    {card.content[0].pharagraphs.map((paragraph, index) => (
-                        <p key={index} className="mb-4 text-slate-100">
+                    {card.content.map((item, index) => (
+                    <div key={index} className="bg-slate-800 w-full rounded-lg p-6 mb-6">
+                        <h2 className="text-white text-2xl font-bold leading-tight mb-4">
+                        {item.subtitle}
+                        </h2>
+                        {item.pharagraphs.map((paragraph, index) => (
+                        <p key={index} className="text-white mb-4">
                             {paragraph}
                         </p>
+                        ))}
+                    </div>
                     ))}
                 </div>
             </div>
